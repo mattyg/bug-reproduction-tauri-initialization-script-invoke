df15bcc8bb436ad87e76f351fcdb288be5fcbf62
Matt Gabrenya <matt@buildyourweb.app>
2025-01-16 10:37:14 -0700

    refactor: simplify example by not using plugins

--- tauri-app/src-tauri/src/lib.rs
+++ tauri-app/src-tauri/src/lib.rs
@@ -1,19 +1,41 @@
 use tauri::{WebviewWindowBuilder, WebviewUrl};
+use log::{SetLoggerError, LevelFilter};
+#[macro_use] extern crate log;
+extern crate simplelog;
+use simplelog::*;
+use tauri_plugin_log::{LogTarget};
+
+#[tauri::command]
+fn greet(name: &str) {
+    log::info!("Hello from {}", name);
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
+
 pub fn run() {
+    CombinedLogger::init(
+        vec![
+            TermLogger::new(LevelFilter::Trace, Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
+       ]
+    ).unwrap();
+
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_log::Builder::default().targets([
+            LogTarget::LogDir,
+            LogTarget::Stdout,
+            LogTarget::Webview,
+        ]).build())
         .setup(|app| {
             let handle = app.handle().clone();
 
             WebviewWindowBuilder::new(&handle, "main", WebviewUrl::App("".into()))
                 .initialization_script(r#"
-                    window.__TAURI_INTERNALS__.invoke("plugin:opener|open_url", {url: "https://google.com"} );
+                    window.__TAURI_INTERNALS__.invoke("greet", {name: "script 1"});
                 "#) 
                 .initialization_script(r#"
                     setTimeout(() => {
-                        window.__TAURI_INTERNALS__.invoke("plugin:opener|open_url", {url: "https://bing.com"} );
+                        window.__TAURI_INTERNALS__.invoke("greet", {name: "script 2"});
                     }, 100);
                 "#)
                 .build()
--- tauri-app/src/routes/+page.svelte
+++ tauri-app/src/routes/+page.svelte
@@ -1,14 +1,5 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/core";
-
-  let name = $state("");
   let greetMsg = $state("");
-
-  async function greet(event: Event) {
-    event.preventDefault();
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsg = await invoke("greet", { name });
-  }
 </script>
 
 <main class="container">
@@ -27,10 +18,6 @@
   </div>
   <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
 
-  <form class="row" onsubmit={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button type="submit">Greet</button>
-  </form>
   <p>{greetMsg}</p>
 </main>
 
