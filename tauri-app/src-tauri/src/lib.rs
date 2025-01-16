c9bed941b72d6864a75d2fb1b5a2fde1acdbd402
Matt Gabrenya <matt@buildyourweb.app>
2025-01-16 10:55:00 -0700

    refactor: simplest possible reproduction

--- tauri-app/src-tauri/src/lib.rs
+++ tauri-app/src-tauri/src/lib.rs
@@ -1,23 +1,13 @@
 use tauri::{WebviewWindowBuilder, WebviewUrl};
-use log::{SetLoggerError, LevelFilter};
-#[macro_use] extern crate log;
-extern crate simplelog;
-use simplelog::*;
 
 #[tauri::command]
-fn greet(name: &str) {
-    log::info!("Hello from {}", name);
+fn greet(name: &str) -> String {
+    format!("Hello from {}", name)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 
 pub fn run() {
-    CombinedLogger::init(
-        vec![
-            TermLogger::new(LevelFilter::Trace, Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
-       ]
-    ).unwrap();
-
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .setup(|app| {
@@ -25,12 +15,7 @@ pub fn run() {
 
             WebviewWindowBuilder::new(&handle, "main", WebviewUrl::App("".into()))
                 .initialization_script(r#"
-                    window.__TAURI_INTERNALS__.invoke("greet", {name: "script 1"});
-                "#) 
-                .initialization_script(r#"
-                    setTimeout(() => {
-                        window.__TAURI_INTERNALS__.invoke("greet", {name: "script 2"});
-                    }, 100);
+                    window.__TAURI_INTERNALS__.invoke("greet", {name: "script 1"}).then((res) => alert(res));
                 "#)
                 .build()
                 .expect("error building webview window");
